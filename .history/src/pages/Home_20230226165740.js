b8e801dc6cd70e5fb652dee4571b7bb081908b23
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2023-02-26 16:58:01 +0200

    Add: add history

---
+++ .history/src/pages/Home_20230226165740.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>My Home Page</div>
+      <Link to="/products">Go to Products Page</Link>
+    </>
+  );
+};
+
+export default HomePage;
