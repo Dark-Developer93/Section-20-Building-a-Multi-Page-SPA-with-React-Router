efee4e49db8b0ea3ea2f65598dc1cdedd7c2b90a
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2023-02-26 17:18:19 +0200

    Add: Layouts & Nested Routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,7 @@
-import { Link } from "react-router-dom";
-
 const HomePage = () => {
   return (
     <>
       <div>My Home Page</div>
-      <Link to="/products">Go to Products Page</Link>
     </>
   );
 };
